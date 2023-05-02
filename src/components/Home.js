7dd0d272841afd9c78968cf03f2ba80123fad411
MdTarikulIslam <nayon1820@gmail.com>
2023-05-02 22:04:38 +0600

    delete added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Home = () => {
   const users = useLoaderData();
+  const [displayUsers, setDisplayUsers] = useState(users);
 
   const handleDelete = (user) => {
     const agree = window.confirm(`are you sure to delete: ${user.name}`);
     console.log(agree);
     if (agree) {
       console.log("deleting user id", user._id);
+      fetch(`http://localhost:5000/users/${user._id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.deletedCount > 0) {
+            alert("User deleted successfully");
+            const remaining = displayUsers.filter(
+              (usr) => usr._id !== user._id
+            );
+            setDisplayUsers(remaining);
+          }
+        });
     }
   };
   return (
     <div>
-      <h1>Users: {users.length}</h1>
+      <h1>Users: {displayUsers.length}</h1>
       <div>
         {users.map((user) => (
           <p key={user._id}>
